0035562b0549a82bf4c29e03b488b9f19f15b25e
Refaza <75516541+Refaza@users.noreply.github.com>
2022-01-26 07:46:39 +0700

    create func genLv, pv LV

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
+'use strict';
 
+let LV = [[]];
+
+function genLv(AS, AE, BS, BE) {
+  let lv = [[]], lvTemp = [];
+  for (let i = AE-AS; i >= 0; i--) {
+    for (let ii = BE-BS; ii >= 0 ; i--) {
+      let lvT = [(AE-i),(BE-ii)];
+      lvT.sort();
+      if(!lvTemp.includes(lvT[0] + "" + lvT[1])) {
+        lv[0].push(lvT[0]);
+        lv[1].push(lvT[1]);
+        lvTemp.push(lvT[0] + "" + lvT[1]);
+      }
+    }
+  }
+  LV = lv;
+}

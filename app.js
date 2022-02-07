783401c4d22da6947d07e56467489e468df4626e
Refaza <75516541+Refaza@users.noreply.github.com>
2022-02-07 17:57:32 +0700

    ADD func randLv, lv22, lv2

--- app.js
+++ app.js
@@ -15,5 +15,53 @@ function genLv(AS, AE, BS, BE) {
       }
     }
   }
-  LV = lv;
+  return lv;
+}
+
+function genLv22(max) {
+  let lv22 = [[],[]];
+  let lv = genLv(2,9,6,9);
+  console.log(LV[0].length);
+  console.log(LV[0]);
+  for (let i=0;i<lv[0].length;i++) {
+    /*console.log("lv");
+    console.log(lv);*/
+    console.log("LV[0]="+lv[0][i]+" LV[1]="+lv[1][i]+" +="+(lv[0][i]+lv[1][i]));
+    if(lv[0][i] + lv[1][i] == max) {
+      console.log("LV2+");
+      lv22[0].push(lv[0][i]);
+      lv22[1].push(lv[1][i]);
+    }
+  }
+  return lv22;
+}
+
+function randLv(lv){
+  //console.log("randQuiz()");
+  let i = lv[0].length, randPos, temp=[];
+  TL.innerText = i;
+  while(--i > 0){
+    randPos = Math.floor(Math.random() * (i+1));
+    console.log("randPos = "+randPos);
+    temp[0] = lv[0][randPos];
+    temp[1] = lv[1][randPos];
+    lv[0][randPos] = lv[0][i];
+    lv[1][randPos] = lv[1][i];
+    lv[0][i] = temp[0];
+    lv[1][i] = temp[1];
+  }
+  return lv;
+}
+
+function genLv2(AS, AE, BS, BE) {
+  let lv2 = [[],[],[]];
+  let lv = randLv(genLv(AS,AE,BS,BE));
+  lv2[0] = lv[0];
+  for(int i=0;i<lv[1].length;i++) {
+    let lv22 = genLv22(lv[1][i]);
+    let randPos = Math.floor(Math.random() * (lv22[0].length+1));
+    lv2[1].push(lv22[0][randPos)]);
+    lv2[3].push(lv22[1][randPos]]);
+  }
+  return lv2;
 }
